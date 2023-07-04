2240783e072a93b03890b3647800725aa625adc7
cener003 <centersoumu0078no3@gmail.com>
2023-07-04 17:56:54 +0900

    7/4

--- footer.php
+++ footer.php
@@ -41,6 +41,10 @@
                     <button><a href="<?php echo home_url(); ?>">ホーム</a></button>
                     <button><a href="<?php echo home_url(); ?>/company">会社一覧</a></button>
                     <button><a href="/faq">よくある質問</a></button>
+
+                    <button><a href="<?php echo home_url(); ?>/#top-question" onclick="checkMenuCheckbox()">リフォームの疑問</a></button>
+
+
                     <button><a href="/company">会社一覧</a></button>
                     <button><a href="/policy">プライバシーポリシー</a></button>
 
@@ -64,7 +68,10 @@
 
 
             </div>
+
             <div class="copyright">
+                <p class="menseki">免責事項：当サイトはお客様と施工業者を仲介するサイトであり、お客様と業者間のトラブルに関していかなる場合も一切の責任を負い兼ねます。<br>また、当サイトの利用により生じた損失や損害については、一切責任を負いません。 </p>
+
                 <small>Copyright &copy;ReformWalker All right reserved.
                 </small>
 
